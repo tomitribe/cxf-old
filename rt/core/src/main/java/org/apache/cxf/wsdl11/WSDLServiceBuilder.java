3c808f7e5bccfa1e2e126e1907e007fe03c42481
J. Daniel Kulp <dkulp@apache.org>
2009-06-30 14:55:48 +0000

    Merged revisions 786642 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.1.x-fixes
    ................
      r786642 | dkulp | 2009-06-19 15:53:40 -0400 (Fri, 19 Jun 2009) | 16 lines
      
      Merged revisions 786640 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/branches/2.2.x-fixes
      
      ................
        r786640 | dkulp | 2009-06-19 15:48:30 -0400 (Fri, 19 Jun 2009) | 9 lines
        
        Merged revisions 786638 via svnmerge from 
        https://svn.apache.org/repos/asf/cxf/trunk
        
        ........
          r786638 | dkulp | 2009-06-19 15:47:08 -0400 (Fri, 19 Jun 2009) | 1 line
          
          [CXF-2295] Add better error message
        ........
      ................
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.0.x-fixes@789776 13f79535-47bb-0310-9956-ffa450edef68

--- rt/core/src/main/java/org/apache/cxf/wsdl11/WSDLServiceBuilder.java
+++ rt/core/src/main/java/org/apache/cxf/wsdl11/WSDLServiceBuilder.java
@@ -509,6 +509,9 @@ public class WSDLServiceBuilder {
         this.copyExtensionAttributes(opInfo, op);
         Input input = op.getInput();
         if (input != null) {
+            if (input.getMessage() == null) {
+                throw new WSDLRuntimeException(LOG, "NO_MESSAGE", "input", op.getName(), input.getName());
+            }
             MessageInfo minfo = opInfo.createMessage(input.getMessage().getQName());
             opInfo.setInput(input.getName(), minfo);
             buildMessage(minfo, input.getMessage());
@@ -517,6 +520,9 @@ public class WSDLServiceBuilder {
         }
         Output output = op.getOutput();
         if (output != null) {
+            if (output.getMessage() == null) {
+                throw new WSDLRuntimeException(LOG, "NO_MESSAGE", "output", op.getName(), output.getName());
+            }
             MessageInfo minfo = opInfo.createMessage(output.getMessage().getQName());
             opInfo.setOutput(output.getName(), minfo);
             buildMessage(minfo, output.getMessage());
