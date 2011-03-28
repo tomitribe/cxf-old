938bdc0cab9c099623532294c6f2a8407d993a03
Willem Ning Jiang <ningjiang@apache.org>
2011-03-28 03:09:35 +0000

    Merged revisions 1086103 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.3.x-fixes
    ................
      r1086103 | ningjiang | 2011-03-28 10:53:07 +0800 (Mon, 28 Mar 2011) | 9 lines
      
      Merged revisions 1086102 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/trunk
      
      ........
        r1086102 | ningjiang | 2011-03-28 10:42:38 +0800 (Mon, 28 Mar 2011) | 1 line
        
        CXF-3426  CXF JMSContinuation leaks threads
      ........
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.2.x-fixes@1086109 13f79535-47bb-0310-9956-ffa450edef68

--- rt/transports/jms/src/main/java/org/apache/cxf/transport/jms/continuations/JMSContinuation.java
+++ rt/transports/jms/src/main/java/org/apache/cxf/transport/jms/continuations/JMSContinuation.java
@@ -135,6 +135,7 @@ public class JMSContinuation implements Continuation {
             public void run() {
                 synchronized (JMSContinuation.this) { 
                     if (isPending) {
+                        cancelTimerTask();
                         doResume();
                     }
                 }
