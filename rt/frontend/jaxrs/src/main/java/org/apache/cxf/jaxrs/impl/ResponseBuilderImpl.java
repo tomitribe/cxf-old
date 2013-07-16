4653b8a75d6c1e23b5c9c45a3ce23b347852b227
Sergey Beryozkin <sergeyb@apache.org>
2013-07-16 11:15:41 +0000

    Merged revisions 1471394,1471499 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1471394 | sergeyb | 2013-04-24 14:04:33 +0100 (Wed, 24 Apr 2013) | 1 line
      
      [CXF-4985] If response entity is null and the status has not ben explicitly set - report 204
    ........
      r1471499 | sergeyb | 2013-04-24 17:09:54 +0100 (Wed, 24 Apr 2013) | 1 line
      
      [CXF-4985] Copying statusSet field when cloning RB
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.7.x-fixes@1503661 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/impl/ResponseBuilderImpl.java
+++ rt/frontend/jaxrs/src/main/java/org/apache/cxf/jaxrs/impl/ResponseBuilderImpl.java
@@ -47,6 +47,7 @@ import org.apache.cxf.phase.PhaseInterceptorChain;
 
 public final class ResponseBuilderImpl extends ResponseBuilder implements Cloneable {
     private int status = 200;
+    private boolean statusSet;
     private Object entity;
     private MultivaluedMap<String, Object> metadata = new MetadataMap<String, Object>();
     private Annotation[] annotations;
@@ -56,11 +57,15 @@ public final class ResponseBuilderImpl extends ResponseBuilder implements Clonea
 
     private ResponseBuilderImpl(ResponseBuilderImpl copy) {
         status = copy.status;
+        statusSet = copy.statusSet;
         metadata.putAll(copy.metadata);
         entity = copy.entity;
     }
        
     public Response build() {
+        if (entity == null && !statusSet) {
+            status = 204;
+        }
         ResponseImpl r = new ResponseImpl(status);
         MetadataMap<String, Object> m = 
             new MetadataMap<String, Object>(metadata, false, true);
@@ -75,6 +80,7 @@ public final class ResponseBuilderImpl extends ResponseBuilder implements Clonea
             throw new IllegalArgumentException("Illegal status value : " + s);
         }
         status = s;
+        statusSet = true;
         return this;
     }
 
