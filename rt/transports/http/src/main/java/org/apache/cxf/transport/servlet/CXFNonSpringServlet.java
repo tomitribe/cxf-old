65f498715d7f62e470cc6b9a4f884e8ec1e551fd
Sergey Beryozkin <sergeyb@apache.org>
2013-07-23 12:21:38 +0000

    Merged revisions 1505957,1505965,1506005 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r1505957 | sergeyb | 2013-07-23 11:15:53 +0100 (Tue, 23 Jul 2013) | 1 line
      
      [CXF-4377] Some more work to do with running CXFServlet as Filter
    ........
      r1505965 | sergeyb | 2013-07-23 11:45:28 +0100 (Tue, 23 Jul 2013) | 1 line
      
      Moving FilterDeclaration package name closer to the relevant code to protect more from pre-3.0 servlet API being linked to; the reflection can be introduced if really needed
    ........
      r1506005 | sergeyb | 2013-07-23 13:05:18 +0100 (Tue, 23 Jul 2013) | 1 line
      
      Using reflection to get FilterRegistration
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.7.x-fixes@1506009 13f79535-47bb-0310-9956-ffa450edef68

--- rt/transports/http/src/main/java/org/apache/cxf/transport/servlet/CXFNonSpringServlet.java
+++ rt/transports/http/src/main/java/org/apache/cxf/transport/servlet/CXFNonSpringServlet.java
@@ -19,14 +19,18 @@
 package org.apache.cxf.transport.servlet;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Collection;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.cxf.Bus;
@@ -34,6 +38,7 @@ import org.apache.cxf.BusException;
 import org.apache.cxf.BusFactory;
 import org.apache.cxf.common.classloader.ClassLoaderUtils;
 import org.apache.cxf.common.classloader.ClassLoaderUtils.ClassLoaderHolder;
+import org.apache.cxf.helpers.CastUtils;
 import org.apache.cxf.resource.ResourceManager;
 import org.apache.cxf.transport.DestinationFactory;
 import org.apache.cxf.transport.DestinationFactoryManager;
@@ -131,7 +136,10 @@ public class CXFNonSpringServlet extends AbstractHTTPServlet {
                 if (bus != null) {
                     origBus = BusFactory.getAndSetThreadDefaultBus(bus);
                 }
-                if (controller.filter((HttpServletRequest)request, (HttpServletResponse)response)) {
+                HttpServletRequest httpRequest = (HttpServletRequest)request;
+                if (controller.filter(new HttpServletRequestFilter(httpRequest,
+                                                                   super.getServletName()),
+                                      (HttpServletResponse)response)) {
                     return;
                 }
             } finally {
@@ -187,6 +195,59 @@ public class CXFNonSpringServlet extends AbstractHTTPServlet {
         if (bus != null) {
             bus.shutdown(true);
             bus = null;
+        }
+    }
+    
+    private static class HttpServletRequestFilter extends HttpServletRequestWrapper {
+        private String filterName;
+        private String servletPath;
+        private String pathInfo;
+        public HttpServletRequestFilter(HttpServletRequest request, String filterName) {
+            super(request);
+            this.filterName = filterName;
+        }
+        
+        @Override
+        public String getServletPath() {
+            if (servletPath == null) {
+                try {
+                    Method m = ServletContext.class.getMethod("getFilterRegistration", new Class[] {String.class});
+                    Object registration = m.invoke(super.getServletContext(), new Object[]{filterName});
+                    if (registration != null) {
+                        m = registration.getClass().getMethod("getUrlPatternMappings", new Class[] {});
+                        Collection<String> mappings = 
+                            CastUtils.cast((Collection<?>)m.invoke(registration, new Object[]{}));
+                        if (!mappings.isEmpty()) {
+                            String mapping = mappings.iterator().next();
+                            if (mapping.endsWith("/*")) {
+                                servletPath = mapping.substring(0, mapping.length() - 2);
+                            }
+                        }
+                    }
+                } catch (Throwable ex) {
+                    // ignore
+                }
+                if (servletPath == null) {
+                    servletPath = "";
+                }
+            }
+            
+            return servletPath;
+        }
+        
+        @Override
+        public String getPathInfo() {
+            if (pathInfo == null) {
+                pathInfo = super.getPathInfo();
+                if (pathInfo == null) {
+                    pathInfo = getRequestURI();
+                }
+                String prefix = super.getContextPath() + this.getServletPath();
+                if (pathInfo.startsWith(prefix)) {
+                    pathInfo = pathInfo.substring(prefix.length());
+                }
+            }
+            return pathInfo;
         }
     }
 }
